5dc9e61c3c5db6f47cf5873f0a4d3ae4b1b7c7f4
Simon Que <simonque@gmail.com>
2014-06-21 17:20:49 -0700

    Fix indentation, remove terminating tabs and extra lines
    Done using:
    "Source -> Correct Indentation" for Java files and
    "Source -> Format Active Elements" for strings.xml

--- src/com/codepath/apps/restclienttemplate/RestClient.java
+++ src/com/codepath/apps/restclienttemplate/RestClient.java
@@ -22,32 +22,32 @@ import com.loopj.android.http.RequestParams;
  * 
  */
 public class RestClient extends OAuthBaseClient {
-    public static final Class<? extends Api> REST_API_CLASS = FlickrApi.class; // Change this
-    public static final String REST_URL = "http://api.flickr.com/services"; // Change this, base API URL
-    public static final String REST_CONSUMER_KEY = "SOME_KEY";       // Change this
-    public static final String REST_CONSUMER_SECRET = "SOME_SECRET"; // Change this
-    public static final String REST_CALLBACK_URL = "oauth://cprest"; // Change this (here and in manifest)
-    
-    public RestClient(Context context) {
-        super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
-    }
-    
-    // CHANGE THIS
-    // DEFINE METHODS for different API endpoints here
-    public void getInterestingnessList(AsyncHttpResponseHandler handler) {
-        String apiUrl = getApiUrl("?nojsoncallback=1&method=flickr.interestingness.getList");
-        // Can specify query string params directly or through RequestParams.
-        RequestParams params = new RequestParams();
-        params.put("format", "json");
-        client.get(apiUrl, params, handler);
-    }
-    
-    /* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
-     * 	  i.e getApiUrl("statuses/home_timeline.json");
-     * 2. Define the parameters to pass to the request (query or body)
-     *    i.e RequestParams params = new RequestParams("foo", "bar");
-     * 3. Define the request method and make a call to the client
-     *    i.e client.get(apiUrl, params, handler);
-     *    i.e client.post(apiUrl, params, handler);
-     */
+	public static final Class<? extends Api> REST_API_CLASS = FlickrApi.class; // Change this
+	public static final String REST_URL = "http://api.flickr.com/services"; // Change this, base API URL
+	public static final String REST_CONSUMER_KEY = "SOME_KEY";       // Change this
+	public static final String REST_CONSUMER_SECRET = "SOME_SECRET"; // Change this
+	public static final String REST_CALLBACK_URL = "oauth://cprest"; // Change this (here and in manifest)
+
+	public RestClient(Context context) {
+		super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
+	}
+
+	// CHANGE THIS
+	// DEFINE METHODS for different API endpoints here
+	public void getInterestingnessList(AsyncHttpResponseHandler handler) {
+		String apiUrl = getApiUrl("?nojsoncallback=1&method=flickr.interestingness.getList");
+		// Can specify query string params directly or through RequestParams.
+		RequestParams params = new RequestParams();
+		params.put("format", "json");
+		client.get(apiUrl, params, handler);
+	}
+
+	/* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
+	 * 	  i.e getApiUrl("statuses/home_timeline.json");
+	 * 2. Define the parameters to pass to the request (query or body)
+	 *    i.e RequestParams params = new RequestParams("foo", "bar");
+	 * 3. Define the request method and make a call to the client
+	 *    i.e client.get(apiUrl, params, handler);
+	 *    i.e client.post(apiUrl, params, handler);
+	 */
 }
