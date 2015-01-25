3717c33659531de5e00f81b8819888b8db8ef753
Nathan Esquenazi <nesquena@gmail.com>
2015-01-25 00:26:47 -0800

    Use Picasso and upgrade to android-async-http

--- app/src/main/java/com/codepath/apps/restclienttemplate/RestClientApp.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/RestClientApp.java
@@ -2,10 +2,6 @@ package com.codepath.apps.restclienttemplate;
 
 import android.content.Context;
 
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-
 /*
  * This is the Android application itself and is used to configure various settings
  * including the image cache in memory and on disk. This also adds a singleton
@@ -22,14 +18,6 @@ public class RestClientApp extends com.activeandroid.app.Application {
 	public void onCreate() {
 		super.onCreate();
 		RestClientApp.context = this;
-
-		// Create global configuration and initialize ImageLoader with this configuration
-		DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().
-				cacheInMemory().cacheOnDisc().build();
-		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext())
-		.defaultDisplayImageOptions(defaultOptions)
-		.build();
-		ImageLoader.getInstance().init(config);
 	}
 
 	public static RestClient getRestClient() {
