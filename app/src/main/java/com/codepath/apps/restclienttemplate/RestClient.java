6897eacd7da2d08933f97ceda59482673725e2ab
Roger Hu <roger.hu@gmail.com>
2017-04-23 00:36:13 -0700

    Bump versions, upgrade to latest Scribe

--- app/src/main/java/com/codepath/apps/restclienttemplate/RestClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/RestClient.java
@@ -1,11 +1,10 @@
 package com.codepath.apps.restclienttemplate;
 
-import org.scribe.builder.api.Api;
-import org.scribe.builder.api.FlickrApi;
-
 import android.content.Context;
 
 import com.codepath.oauth.OAuthBaseClient;
+import com.github.scribejava.apis.FlickrApi;
+import com.github.scribejava.core.builder.api.BaseApi;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
@@ -22,14 +21,14 @@ import com.loopj.android.http.RequestParams;
  * 
  */
 public class RestClient extends OAuthBaseClient {
-	public static final Class<? extends Api> REST_API_CLASS = FlickrApi.class; // Change this
+	public static final BaseApi REST_API_INSTANCE = FlickrApi.instance(); // Change this
 	public static final String REST_URL = "https://api.flickr.com/services"; // Change this, base API URL
 	public static final String REST_CONSUMER_KEY = "SOME_KEY";       // Change this
 	public static final String REST_CONSUMER_SECRET = "SOME_SECRET"; // Change this
 	public static final String REST_CALLBACK_URL = "oauth://cprest"; // Change this (here and in manifest)
 
 	public RestClient(Context context) {
-		super(context, REST_API_CLASS, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
+		super(context, REST_API_INSTANCE, REST_URL, REST_CONSUMER_KEY, REST_CONSUMER_SECRET, REST_CALLBACK_URL);
 	}
 
 	// CHANGE THIS
