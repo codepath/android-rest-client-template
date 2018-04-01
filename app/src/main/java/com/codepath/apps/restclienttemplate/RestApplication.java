41ebad8f674bb34b1d034b7724383d3dc56628f7
Roger Hu <rhu@hearsaycorp.com>
2018-03-31 23:54:44 -0700

    Get rid of static Context reference, linter complains

--- app/src/main/java/com/codepath/apps/restclienttemplate/RestApplication.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/RestApplication.java
@@ -17,7 +17,6 @@ import android.content.Context;
  *
  */
 public class RestApplication extends Application {
-	private static Context context;
 
 	@Override
 	public void onCreate() {
@@ -25,11 +24,9 @@ public class RestApplication extends Application {
 
 		FlowManager.init(new FlowConfig.Builder(this).build());
 		FlowLog.setMinimumLoggingLevel(FlowLog.Level.V);
-
-		RestApplication.context = this;
 	}
 
-	public static RestClient getRestClient() {
-		return (RestClient) RestClient.getInstance(RestClient.class, RestApplication.context);
+	public static RestClient getRestClient(Context context) {
+		return (RestClient) RestClient.getInstance(RestClient.class, context);
 	}
 }
