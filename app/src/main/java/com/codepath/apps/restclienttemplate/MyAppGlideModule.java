fc8eefab1d9a4443502dfc52c1d8b8efd84b7d2f
Roger Hu <roger@codepath.com>
2019-06-03 00:46:29 -0700

    downgrade for now to avoid confusion

--- app/src/main/java/com/codepath/apps/restclienttemplate/MyAppGlideModule.java
+++
@@ -1,7 +0,0 @@
-package com.codepath.apps.restclienttemplate;
-import com.bumptech.glide.module.AppGlideModule;
-
-public class MyAppGlideModule extends AppGlideModule {
-    // leave empty for now
-}
-
